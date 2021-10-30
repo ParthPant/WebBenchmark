2f9961cce4322d2d92d1cf113d0dacd942d4233e
Parth Pant <parthpant4@gmail.com>
2021-10-30 23:35:36 +0530

    nginx

--- server/profiler/src/App.hpp
+++ server/profiler/src/App.hpp
@@ -4,16 +4,26 @@
 #define FUNCTION(fn, ...) std::bind(&App::fn, this, ## __VA_ARGS__)
 
 class App {
-    int fact(int i) {
+    int fact_rec(int i) {
         if (i <= 0) return -1;
         else if (i == 1) return i;
-        else return i * fact(i-1);
+        else return i * fact_rec(i-1);
+    }
+
+    int fact_loop(int i) {
+        int res = 1;
+        while (i > 1) {
+            res *= i;
+            i--;
+        }
+        return res;
     }
 
 public:
     void Run()
     {
-        PROFILE("factorial", 500, FUNCTION(fact, 50))
+        PROFILE("factorial_rec", 500, FUNCTION(fact_rec, 50))
+        PROFILE("factorial_loop", 500, FUNCTION(fact_loop, 50))
     }
 
     static App Get()
